1e756bbf898676a9bb4104499b03f4bdc49cdfc2
Setich2 <Shikaa577@gmail.com>
2022-03-30 14:30:44 +0300

    Added new line and rules

--- src/com/gmailShikaa577/Main.java
+++ src/com/gmailShikaa577/Main.java
@@ -3,6 +3,7 @@ package com.gmailShikaa577;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Привет, как дела");
+        System.out.println("Привет, как дела?");
+        System.out.println("- Все хорошо");
     }
 }
