ba525e30be24ecbc36277c3c5b80ce368c60bcca
Setich2 <Shikaa577@gmail.com>
2022-03-30 14:12:51 +0300

    change words

--- src/com/gmailShikaa577/Main.java
+++ src/com/gmailShikaa577/Main.java
@@ -3,6 +3,6 @@ package com.gmailShikaa577;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Привет, пока");
+        System.out.println("Привет, как дела");
     }
 }
