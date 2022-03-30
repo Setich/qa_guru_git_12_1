a6e5d18ebe3f509198cd91c0da96386625e9baa5
Setich2 <Shikaa577@gmail.com>
2022-03-30 15:01:34 +0300

    Added new line and rules about slow branch

--- src/com/gmailShikaa577/Main.java
+++ src/com/gmailShikaa577/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, как дела?");
         System.out.println("- Все хорошо");
+        System.out.println("Точно?");
     }
 }
